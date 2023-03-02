5caa21f5e5f87781de552d717a51473ef267fb60
James McBeath <jamesmcbeath@me.com>
2023-03-02 18:58:22 +0000

    Initial commit with pseudo code

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Structure for code
+Api function
+
+formbox addevent listener on keypress 
+
+function for query (input details are correct)
+
+if/else for form validation
+
+function to get results from API for weather variable
+
+function to display results
+date
+temp
+weather
+temprange
+
+create arrays for months and days
+
+function for dates, year etc.
+
+*/
