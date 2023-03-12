b4b558e514bebe5ec1329f9573f15952a5b8846c
James McBeath <jamesmcbeath@me.com>
2023-03-12 14:36:45 +0000

    ESLint error correct with double quotation marks

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-/* Structure for code
-Api function
-
-formbox addevent listener on keypress 
-
-function for query (input details are correct)
-
-add validation code 
-
-if/else for form validation
-
-function to get results from API for weather variable
-
-function to display results
-date
-temp
-weather
-temprange
-
-create arrays for months and days
-
-function for dates, year etc.
-
-*/
-
 const api = {
     key: "8afe3279994cbd73dbc74066c796d2b9",
     baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
-const formBox = document.querySelector('.form-control');
-formBox.addEventListener('keypress', setQuery);
+const formBox = document.querySelector(".form-control");
+formBox.addEventListener("keypress", setQuery);
 
 function setQuery(e)   {
     if (e.keyCode === 13) {
@@ -45,16 +20,16 @@ function getResults (query) {
   }
   
   function displayResults (weather) {
-    let city = document.querySelector('.location .city');
+    let city = document.querySelector(".location .city");
     city.innerText = `${weather.name}, ${weather.sys.country}`
     let now = new Date();
-    let date = document.querySelector('.location .date');
+    let date = document.querySelector(".location .date");
     date.innerText = dateBuilder(now);
-    let temp = document.querySelector('.temp');
+    let temp = document.querySelector(".temp");
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-    let weather_el = document.querySelector('.weather-type'); 
+    let weather_el = document.querySelector(".weather-type"); 
     weather_el.innerText = weather.weather[0].main; 
-    let tempRange = document.querySelector('.temp-range'); 
+    let tempRange = document.querySelector(".temp-range"); 
     tempRange.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
   }
   
