b1bcb08824b6334c531b138c360972a78f16da8a
James McBeath <jamesmcbeath@me.com>
2023-03-12 17:19:06 +0000

    Resolved JS Lint issues added

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const api = {
-    key: "8afe3279994cbd73dbc74066c796d2b9",
-    baseurl: "https://api.openweathermap.org/data/2.5/"
+  baseurl: "https://api.openweathermap.org/data/2.5/",
+  key: "8afe3279994cbd73dbc74066c796d2b9", 
 }
-
 const formBox = document.querySelector(".form-control");
 formBox.addEventListener("keypress", setQuery);
 
@@ -11,15 +10,13 @@ function setQuery(e)   {
         getResults(formBox. value);
     }
 }
-
 function getResults (query) { 
     fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(weather => { //code taken from stackoverflow. Amended after manual test. Error found with api.base - should have been baseurl
+      .then(weather => {
         return weather.json();
       }).then(displayResults);
-  }
-  
-  function displayResults (weather) {
+}
+function displayResults (weather) {
     let city = document.querySelector(".location .city");
     city.innerText = `${weather.name}, ${weather.sys.country}`
     let now = new Date();
@@ -31,17 +28,14 @@ function getResults (query) {
     weather_el.innerText = weather.weather[0].main; 
     let tempRange = document.querySelector(".temp-range"); 
     tempRange.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-  }
-  
+}
   function dateBuilder (d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-  
     return `${day} ${date} ${month} ${year}`;
-  }
+}
   
